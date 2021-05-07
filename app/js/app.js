5941c4af726fde82d62e9109cd79e24e1f95df75
Anastasiia <nsydorchenko@gmail.com>
2021-05-07 13:13:25 +0300

    scroll down button fix

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,5 @@
 // // Import jQuery module (npm i jquery)
 import $ from 'jquery'
-import { createPopper } from '@popperjs/core'
-import bootstrap from 'bootstrap'
-
-// window.jQuery = $
-// window.$ = $
-
-// // Import vendor jQuery plugin example (not module)
-// require('~/app/libs/mmenu/dist/mmenu.js')
-
-// 100VH BAG FIX on mobile 
-// ENG https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
-// RUS https://denis-creative.com/zadaem-razmer-100vh-bez-prokrutki-dlya-mobilnyh-ustrojstv/
 
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
@@ -37,18 +25,3 @@ $(document).ready(function () {
       $('body').removeClass('lock'); // close burger menu after click
    });
 });
-
-// // STICKY MENU
-// $(document).ready(function () {
-//    const navOffset = $('#nav').offset().top;
-   
-//    $(window).scroll(function () {
-//       const scrolled = $(this).scrollTop();
-
-//       if (scrolled > navOffset) {
-//          $('#wrapper').addClass('nav-fixed');
-//       } else if (scrolled < navOffset) {
-//          $('#wrapper').removeClass('nav-fixed'); 
-//       }
-//    });
-// });
